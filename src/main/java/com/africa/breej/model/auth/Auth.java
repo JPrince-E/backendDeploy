b1ebd6ebb9a9c5f2feeda3c754b0d72ce6ca94a3
JPrince-E <adebayoea1@gmail.com>
2023-07-03 00:08:25 +0100

    commit

--- src/main/java/com/africa/breej/model/auth/Auth.java
+++ src/main/java/com/africa/breej/model/auth/Auth.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.constraints.Email;
+
 @Getter
 @Setter
 @Document(collection = "project_breej_db_auth")
